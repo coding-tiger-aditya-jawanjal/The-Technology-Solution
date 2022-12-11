b06a8d9c410db6b3ff9b2717cf48c60588e2a5c5
Aditya Jawanjal <adityajawanjal01@gmail.com>
2022-12-11 18:24:10 +0530

    done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,10 +13,13 @@ const About = () => {
           borderTopRightRadius={{ md: "150px" }}
           borderBottomRightRadius={{ md: "150px" }}
         >
-          <Heading as={"h3"} fontSize={"2rem"} mb={"5"}>
+          <Heading as={"h3"} fontSize={{ base: "1rem", sm: "1.2rem" }} mb={"5"}>
             About Us
           </Heading>
-          <Text fontSize={"1.2rem"} textAlign={{ md: "justify" }}>
+          <Text
+            fontSize={{ base: "0.5rem", sm: "1.2rem" }}
+            textAlign={{ md: "justify" }}
+          >
             We are the leading software developers in our country. Feel free to
             connect me for work. I am also available for remote work and
             freelancing. This project is done using React.Js and Chakra UI.
